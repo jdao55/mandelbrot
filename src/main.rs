12b66d1d3efd4779f4b384de9ef29bab19385529
joseph dao <joedao55@gmail.com>
2020-08-26 17:10:00 -0400

    clap argparsing, and julia set

--- src/main.rs
+++ src/main.rs
@@ -1,87 +1,63 @@
 use num::Complex;
-use rayon::prelude::*;
 
-use std::str::FromStr;
-
-//TODO add rayon
-fn parse_pair<T: FromStr>(s: &str, separator: char) -> Option<(T, T)> {
-    match s.find(separator) {
-        None => None,
-        Some(index) => {
-            match (T::from_str(&s[..index]), T::from_str(&s[index + 1..])) {
-                (Ok(l), Ok(r)) => Some((l, r)),
-                _ => None
-            }
-        }
-    }
-}
-
-
-
-use image::ColorType;
 use image::png::PNGEncoder;
+use image::ColorType;
 use std::fs::File;
 
 /// Write the buffer `pixels`, whose dimensions are given by `bounds`, to the
 /// file named `filename`.
-fn write_image(filename: &str, pixels: &[u8],
-               bounds: (usize, usize))
-    -> Result<(), std::io::Error>
-{
+fn write_image(filename: &str, pixels: &[u8], x: u32, y: u32) -> Result<(), std::io::Error> {
     let output = File::create(filename)?;
 
     let encoder = PNGEncoder::new(output);
-    encoder.encode(&pixels,
-                   bounds.0 as u32, bounds.1 as u32,
-                   ColorType::Gray(8))?;
+    encoder.encode(&pixels, x, y, ColorType::Gray(8))?;
 
     Ok(())
 }
+fn complex_point(x: u32, y: u32, R: f32, dim: (u32, u32)) -> Complex<f32> {
+    Complex {
+        re: (x as f32) / (dim.0 as f32) * 2.0 - 1.0,
+        im: (y as f32) / (dim.1 as f32) * 2.0 * R - R,
+    }
+}
+fn value(x: u32, y: u32, max_x: u32, max_y: u32, R: f32) -> u8 {
+    let c = Complex {
+        re: 0.285,
+        im: 0.01,
+    };
+    let mut z = complex_point(x, y, R, (max_x, max_y)) * 1.5;
 
-fn value(x: u32, y: u32, bounds: (usize, usize) ) -> u8 {
-    let x = x as f32;
-    let y = y as f32;
-    let a = bounds.0 as f32;
-    let b = bounds.1 as f32;
-    let point = Complex {re: x/a - 1.5, im: y/b -0.5};
-    let mut z = Complex {re: 0 as f32, im: 0 as f32};
-
-    for i in 0..255 {
-        z = z * z + point;
+    for i in 0..64 {
+        z = z * z + c;
         if z.norm_sqr() > 4.0 {
-            return i as u8;
+            return 255 - ((255.0 * (i as f32 / 64.0)) as u8);
         }
     }
     0
-
-
 }
-use std::io::Write;
+
+use clap::Clap;
+
+#[derive(Clap)]
+#[clap(version = "1.0")]
+struct Args {
+    file: String,
+    x: u32,
+    y: u32,
+}
 
 fn main() {
-    let args: Vec<String> = std::env::args().collect();
+    let args: Args = Args::parse();
 
-    if args.len() != 3 {
-        writeln!(std::io::stderr(),
-                 "Usage: mandelbrot filename dimensions(1200x800) ")
-            .unwrap();
-        std::process::exit(1);
-    }
-
-    let bounds: (usize, usize) = parse_pair(&args[2], 'x')
-        .expect("error parsing image dimensions");
-
-    let mut pixels = vec![0; bounds.0 * bounds.1];
+    let mut pixels = vec![0; (args.x * args.y) as usize];
 
     let mut n = 0;
-    let nox = bounds.0 as u32;
-    let noy =  bounds.1 as u32;
-    for x in 0..nox{
-        for y in 0..noy{
-            pixels[n]=value(x, y, bounds);
+    let r = args.y as f32 / args.x as f32;
+    for y in 0..args.y {
+        for x in 0..args.x {
+            pixels[n] = value(x, y, args.x, args.y, r);
             n = n + 1;
         }
     }
-
-    write_image(&args[1], &pixels, bounds).expect("error writing PNG file");
+    write_image(&args.file, &pixels, args.x, args.y).expect("error writing PNG file");
 }
